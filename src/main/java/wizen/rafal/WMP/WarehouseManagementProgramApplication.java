8a3abb353eb6a3da133b3c2518cdc6872aa725a2
Rafal Wizen <rafal.wizen@gmail.com>
2020-06-12 21:00:31 +0200

    first commit

---
+++ src/main/java/wizen/rafal/WMP/WarehouseManagementProgramApplication.java
@@ -0,0 +1,13 @@
+package wizen.rafal.WMP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseManagementProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseManagementProgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wizen/rafal/WMP/WarehouseManagementProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package wizen.rafal.WMP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManagementProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
