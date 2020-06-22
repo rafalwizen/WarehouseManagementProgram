25fda3955bf1369eadea0b7f2dc992170c1c9dd8
Rafal Wizen <rafal.wizen@gmail.com>
2020-06-22 20:14:59 +0200

    delete manufacturere service,dao and other unnecessary classes and interfaces

--- src/main/java/wizen/rafal/WMP/rest/ItemRestController.java
+++ src/main/java/wizen/rafal/WMP/rest/ItemRestController.java
@@ -10,19 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 import wizen.rafal.WMP.entity.Item;
 import wizen.rafal.WMP.entity.Manufacturer;
 import wizen.rafal.WMP.service.ItemService;
-import wizen.rafal.WMP.service.ManufacturerService;
 
 @RestController
 @RequestMapping("/api")
 public class ItemRestController {
 
 	private ItemService itemService;
-	private ManufacturerService manufacturerService;
 
 	@Autowired
-	public ItemRestController(ItemService itemService, ManufacturerService manufacturerService) {
+	public ItemRestController(ItemService itemService) {
 		this.itemService = itemService;
-		this.manufacturerService = manufacturerService;
 	}
 	
 	@GetMapping("/items")
@@ -32,6 +29,6 @@ public class ItemRestController {
 	
 	@GetMapping("/manufacturers")
 	public List<Manufacturer> getManufacturersList() {
-		return manufacturerService.findAll();
+		return itemService.findAll(Manufacturer.class);
 	}
 }
